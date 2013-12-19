1bfde044170ad2a41b80451e9d96bd85aedc0681
Derrill Farley <derrill_farley@yahoo.com>
2013-12-18 18:32:54 -0800

    Added AppController

---
+++ Advanced Contact Manager/AppController.h
@@ -0,0 +1,13 @@
+//
+//  AppController.h
+//  Advanced Contact Manager
+//
+//  Created by Derrill Farley on 12/18/13.
+//  Copyright (c) 2013 Trinity Creative Solutions, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppController : NSObject
+
+@end
---
+++ Advanced Contact Manager/AppController.m
@@ -0,0 +1,13 @@
+//
+//  AppController.m
+//  Advanced Contact Manager
+//
+//  Created by Derrill Farley on 12/18/13.
+//  Copyright (c) 2013 Trinity Creative Solutions, LLC. All rights reserved.
+//
+
+#import "AppController.h"
+
+@implementation AppController
+
+@end
