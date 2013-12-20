b56ff5b72baad2a7870c8621ad9e056e6dccbaa5
Derrill Farley <derrill_farley@yahoo.com>
2013-12-20 04:09:46 -0800

    Added boilerplate copyright stuff.

--- Advanced Contact ManagerTests/Advanced_Contact_ManagerTests.m
+++ Advanced Contact ManagerTests/Advanced_Contact_ManagerTests.m
@@ -1,3 +1,20 @@
+///////////////////////////////////////////////////////////////////////////
+// TRINITY CREATIVE SOLUTIONS, LLC. CONFIDENTIAL
+// __________________
+//
+//  [2007] - [2013] Trinity Creative Solutions, LLC
+//  All Rights Reserved.
+//
+// NOTICE:  All information contained herein is, and remains
+// the property of Trinity Creative Solutions, LLC and its suppliers,
+// if any.  The intellectual and technical concepts contained
+// herein are proprietary to Trinity Creative Solutions, LLC
+// and its suppliers and may be covered by U.S. and Foreign Patents,
+// patents in process, and are protected by trade secret or copyright law.
+// Dissemination of this information or reproduction of this material
+// is strictly forbidden unless prior written permission is obtained
+// from Trinity Creative Solutions, LLC.
+//
 //
 //  Advanced_Contact_ManagerTests.m
 //  Advanced Contact ManagerTests
